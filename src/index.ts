85ddb95ed13dd06f956edf6f150b3345c9e6a290
Simon <sstur@me.com>
2023-01-24 20:38:06 -0800

    Add playground

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,20 @@ import express from 'express';
 
 import * as handlers from './routes';
 import { attachRoutes } from './server';
+import { loadPlayground } from './playground/loadPlayground';
 
 const PORT = 4000;
 
 const app = express();
 app.disable('x-powered-by');
+loadPlayground(app);
 
 const middleware = attachRoutes(...Object.values(handlers));
 app.use(middleware);
+
+app.get('/', (request, response) => {
+  response.send(`<p>Open the <a href="/playground">REST Playground</a></p>`);
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
