bcc2def54ad10779e35d054f2ab19b59586c2b34
Simon <sstur@me.com>
2023-01-24 23:18:56 -0800

    Update routes/index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,5 @@ export { default as auth } from './auth';
 export { default as comments } from './comments';
 export { default as genres } from './genres';
 export { default as movies } from './movies';
+export { default as movie } from './movie';
 export { default as signup } from './signup';
