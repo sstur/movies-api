a63b2c1f16b4b55eafdea1469094a30ac9fc14a6
Simon <sstur@me.com>
2023-01-24 23:14:55 -0800

    Add signup route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,3 +2,4 @@ export { default as auth } from './auth';
 export { default as comments } from './comments';
 export { default as genres } from './genres';
 export { default as movies } from './movies';
+export { default as signup } from './signup';
---
+++ src/routes/signup.ts
@@ -0,0 +1,37 @@
+import { HttpError, Response } from '@nbit/express';
+import { Record, String } from 'runtypes';
+
+import { db } from '../db';
+import { defineRoutes } from '../server';
+
+const Body = Record({
+  name: String,
+  username: String,
+  password: String,
+});
+
+export default defineRoutes((app) => [
+  app.post('/signup', async (request) => {
+    const body = await request.json();
+    if (!Body.guard(body)) {
+      throw new HttpError({ status: 400 });
+    }
+    const { name, username, password } = body;
+    const users = await db.User.findWhere((user) => user.username === username);
+    if (users.length) {
+      return Response.json(
+        { success: false, error: 'Username not available' },
+        { status: 400 },
+      );
+    }
+    const user = await db.User.insert({
+      name,
+      username,
+      password,
+      favorites: [],
+    });
+    const now = new Date().toISOString();
+    const session = await db.Session.insert({ user: user.id, createdAt: now });
+    return { success: true, user, token: session.id };
+  }),
+]);
