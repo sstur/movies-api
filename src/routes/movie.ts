b9c481af04f34a9ce3a619171d1bddd7b03dab9a
Simon <sstur@me.com>
2023-01-24 22:56:43 -0800

    Remove some extraneous code

--- src/routes/movie.ts
+++ src/routes/movie.ts
@@ -3,49 +3,6 @@ import { HttpError } from '@nbit/express';
 import { db } from '../db';
 import { defineRoutes } from '../server';
 
-// type ApiMovie = {
-//   id: number;
-//   title: string;
-//   // tagline: string;
-//   overview: string;
-//   release_date: string;
-//   // runtime: number;
-//   genres: Array<{ id: number; name: string }>;
-//   popularity: number;
-//   vote_average: number;
-//   poster_path: string;
-//   backdrop_path: string;
-//   // status: string;
-//   // homepage: string;
-// };
-
-// function toMovie(input: ApiMovie): Movie {
-//   const {
-//     id,
-//     title,
-//     overview,
-//     release_date,
-//     genres,
-//     popularity,
-//     vote_average,
-//     poster_path,
-//     backdrop_path,
-//   } = input;
-//   return {
-//     id: 'm' + String(id),
-//     title: title,
-//     overview: overview,
-//     release_date: release_date,
-//     genres: genres.map(({ id }) => 'g' + String(id)),
-//     popularity: popularity,
-//     vote_average: vote_average,
-//     poster_path: poster_path,
-//     backdrop_path: backdrop_path,
-//     favoritedBy: [],
-//     comments: [],
-//   };
-// }
-
 export default defineRoutes((app) => [
   app.get('/movies/:id', async (request) => {
     const id = request.params.id;
