7df6e7d44dfe759d6f9bb07e2f3b82ff54b876c2
Simon <sstur@me.com>
2023-01-24 22:48:53 -0800

    Insert genre if not exists

--- src/routes/genres.ts
+++ src/routes/genres.ts
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 
+import { db } from '../db';
 import { defineRoutes } from '../server';
 import { TMDB_API_KEY } from '../support/constants';
 import { memoize } from '../support/memoize';
@@ -15,11 +16,17 @@ const getGenres = memoize(async (): Promise<Array<Genre>> => {
   url.searchParams.set('api_key', TMDB_API_KEY);
   const response = await fetch(url.toString());
   const data = await response.json();
-  const results = data.genres as Array<ApiGenre>;
-  return results.map(({ id, name }) => ({
-    id: 'g' + String(id),
-    name,
-  }));
+  const genres = data.genres as Array<ApiGenre>;
+  const results: Array<Genre> = [];
+  for (const { id, name } of genres) {
+    const genre: Genre = { id: 'g' + String(id), name };
+    const existing = await db.Genre.getById(genre.id);
+    if (!existing) {
+      await db.Genre.insert(genre);
+    }
+    results.push(genre);
+  }
+  return results;
 });
 
 export default defineRoutes((app) => [
