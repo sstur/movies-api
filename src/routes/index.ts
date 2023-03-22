ab110059ed83eb66ab88690b2c99e85836a3bf2f
Simon <sstur@me.com>
2023-03-22 07:55:11 -0700

    Consolidate movie.ts and movies.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 export { default as auth } from './auth';
 export { default as comments } from './comments';
 export { default as genres } from './genres';
-export { default as movie } from './movie';
 export { default as movies } from './movies';
 export { default as signup } from './signup';
 export { default as user } from './user';
